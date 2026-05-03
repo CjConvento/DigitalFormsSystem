42fc11f84b297f4decaf8e70318ccc3a796abfdc
Dang CJ <53930227+CjConvento@users.noreply.github.com>
2026-05-03 14:08:12 +0800

    fixed delete not working and prepared datetime especially the time picker to be fixed

---
+++ wwwroot/js/edit-formdatetime.js
@@ -0,0 +1,22 @@
+﻿// edit-formdatetime.js
+document.querySelector('form').addEventListener('submit', function () {
+    // Incident
+    var incidentDate = document.getElementById('IncidentDate').value;
+    var incidentTime = document.getElementById('IncidentTime').value;
+    var incidentHidden = document.querySelector('[name="IncidentDateTime"]');
+    if (incidentDate && incidentTime) {
+        incidentHidden.value = incidentDate + 'T' + incidentTime;
+    } else {
+        incidentHidden.value = '';
+    }
+
+    // Received
+    var receivedDate = document.getElementById('ReceivedDate').value;
+    var receivedTime = document.getElementById('ReceivedTime').value;
+    var receivedHidden = document.querySelector('[name="ReceivedDateTime"]');
+    if (receivedDate && receivedTime) {
+        receivedHidden.value = receivedDate + 'T' + receivedTime;
+    } else {
+        receivedHidden.value = '';
+    }
+});
