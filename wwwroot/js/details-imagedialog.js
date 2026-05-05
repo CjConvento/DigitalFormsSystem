f7c8a8cb79ab043abeebec6af27f649146ac4951
CjConvento <conventocj110@gmail.com>
2026-05-05 10:49:13 +0800

    feat(DamagedReport): add image modal on Details page click

---
+++ wwwroot/js/details-imagedialog.js
@@ -0,0 +1,15 @@
+﻿// details-imagedialog.js
+$(document).ready(function () {
+    // Attach click event to all images with class 'clickable-image'
+    $('.clickable-image').on('click', function () {
+        var fullSrc = $(this).data('fullsrc');
+        var fileName = $(this).data('filename');
+        if (fullSrc) {
+            $('#modalImage').attr('src', fullSrc);
+            $('#imageModalLabel').text(fileName || 'Image Preview');
+            // Bootstrap 5 modal show
+            var myModal = new bootstrap.Modal(document.getElementById('imageModal'));
+            myModal.show();
+        }
+    });
+});
